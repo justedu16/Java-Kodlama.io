67e9669454cd7b03212f08e6454d61ade513137b
Enes <75938150+justedu16@users.noreply.github.com>
2022-09-17 01:48:55 +0300

    Add files via upload

---
+++ switchh.java
@@ -0,0 +1,28 @@
+
+public class switchh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char grade = 'F';
+
+		switch (grade) {
+		case 'A':
+			System.out.println("Harika");
+			break;
+		case 'B':
+			System.out.println("Fena degil");
+			break;
+		case 'C':
+			System.out.println("İyi");
+			break;
+		case 'F':
+			System.out.println("Basarisiz");
+			break;
+		default:
+			System.out.println("Gecersiz not");
+
+		}
+
+	}
+
+}
