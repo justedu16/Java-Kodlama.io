011ca4a89a767cb82290cc5b5316696880a20c91
Enes <75938150+justedu16@users.noreply.github.com>
2022-09-16 17:55:40 +0300

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mesaj = "Ogrenci sayisi :";
+		int sayi = 16;
+		System.out.println(mesaj + sayi);
+				
+				
+
+	}
+
+}
