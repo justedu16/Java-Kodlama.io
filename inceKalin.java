a50b821b328e1474efdcf258894a9eebc58e642e
Enes <75938150+justedu16@users.noreply.github.com>
2022-09-19 17:22:56 +0300

    Add files via upload

---
+++ inceKalin.java
@@ -0,0 +1,41 @@
+package inceHarfKalinHarf;
+
+public class inceKalin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char table = 'e';
+		char harf;
+		harf = Character.toUpperCase(table);
+		
+			
+		
+		if(harf == 'A') {
+			System.out.println("Kalin");
+		}
+		else if(harf == 'I') {
+			System.out.println("Kalin");
+		}
+		else if(harf == 'U') {
+			System.out.println("Kalin");
+		}
+		else if(harf == 'O') {
+			System.out.println("Kalin");
+		}
+		else if(harf == 'E') {
+			System.out.println("Ince");
+		}
+		else if(harf == 'İ') {
+			System.out.println("Ince");
+		}
+		else if(harf == 'Ü') {
+			System.out.println("Ince");
+		}
+		else if(harf == 'Ö') {
+			System.out.println("Ince");
+		}
+		else {System.out.println("Tanimsiz");}
+
+	}
+
+}
